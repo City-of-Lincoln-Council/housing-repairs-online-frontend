0d5bfaf7855e11f6a890ab745896bf88b8084a13
maysakanoni <maysa@madetech.com>
2021-11-11 10:55:15 +0000

    update emergency repair page

--- src/components/reportRepair/emergencyRepair.js
+++ src/components/reportRepair/emergencyRepair.js
@@ -1,4 +1,5 @@
-import { H1, H3, GridRow, GridCol, Paragraph } from 'govuk-react';
+import { List } from '@govuk-react/tabs';
+import { H1, H3, GridRow, GridCol, Paragraph, UnorderedList, ListItem } from 'govuk-react';
 
 const EmergencyRepair = () => {
   return (
@@ -14,15 +15,26 @@ const EmergencyRepair = () => {
         <Paragraph>
           Emergency Out of Hours Repairs - Telephone: **01522 534747**
         </Paragraph>
-        <Paragraph>For non-emergency requests, call **01522 873333**</Paragraph>
+        <Paragraph>For emergencies in normal hours, please call **01522 873333**</Paragraph>
         <Paragraph>
-          Please do not call the emergency out of hours number if the repair is
-          not urgent. We may charge you a fee if the repair is not deemed an
-          emergency.
+          Opening times:
+        </Paragraph>
+        <UnorderedList>
+          <ListItem>Monday 9am - 5pm</ListItem>
+          <ListItem>Tuesday 9am - 5pm</ListItem>
+          <ListItem>Wednesday 10am - 4.30pm</ListItem>
+          <ListItem>Thursday 9am - 5pm</ListItem>
+          <ListItem>Friday 9am - 4.30pm</ListItem>
+        </UnorderedList>
+        <Paragraph>
+          For non-emergency requests, call **01522 873 333**
+        </Paragraph>
+        <Paragraph>
+          Please do not call the emergency out of hours number if the repair is not urgent.
         </Paragraph>
         <Paragraph>
           If you suspect you have a gas leak, you must report it immediately to
-          the Gas Emergency Service 0800 111 999 or via textphone (minicom) on
+          the Gas Emergency Service **0800 111 999** or via textphone (minicom) on
           **0800 371 787**
         </Paragraph>
       </GridCol>
