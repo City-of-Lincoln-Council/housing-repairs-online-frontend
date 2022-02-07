1d07df0aacf180bc182bd66f16e975d0f208f0f9
el <elena@madetech.com>
2022-02-07 11:41:41 +0000

    stop including sentryWebpackPluginOptions

--- next.config.js
+++ next.config.js
@@ -28,19 +28,20 @@ const moduleExports =  {
   }
 };
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  authToken: process.env.SENTRY_TOKEN,
-  ignore: ['node_modules'],
-  project: 'housing-repairs-online-frontend',
+// const sentryWebpackPluginOptions = {
+//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
+//   // the following options are set automatically, and overriding them is not
+//   // recommended:
+//   //   release, url, org, project, authToken, configFile, stripPrefix,
+//   //   urlPrefix, include, ignore
+//   authToken: process.env.SENTRY_TOKEN,
+//   ignore: ['node_modules'],
+//   project: 'housing-repairs-online-frontend',
+//
+//   silent: false, // Suppresses all logs
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options.
+// };
 
-  silent: false, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = withSentryConfig(moduleExports);
+// module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
