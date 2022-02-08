caebea30974e51b38e14ce684b903a490dba19f5
el <elena@madetech.com>
2022-02-08 12:51:25 +0000

    add next-source maps

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 const withSass = require('@zeit/next-sass');
+const nextSourceMaps = require('@zeit/next-source-maps');
 
 const { withSentryConfig } = require('@sentry/nextjs');
 
-const moduleExports =  {
+const moduleExports =  nextSourceMaps({
   /* Add Your Scss File Folder Path Here */
   // cssModules: true,
 
@@ -31,22 +32,6 @@ const moduleExports =  {
     disableServerWebpackPlugin: true,
     disableClientWebpackPlugin: true,
   },
-};
+});
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  authToken: process.env.SENTRY_TOKEN,
-  ignore: ['node_modules'],
-  project: 'housing-repairs-online-frontend',
-
-  silent: false, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-// module.exports = withSentryConfig(moduleExports);
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = withSentryConfig(moduleExports);
--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import * as Sentry from '@sentry/nextjs';
 
-export default function Custom404() {
+// export default function Custom404() {
+//
+//   Sentry.captureMessage('404');
+//   Sentry.captureException(new Error('404 hit'));
+//
+//
+//   return <h1>404 - Page Not Found</h1>
+// }
 
-  Sentry.captureMessage('404');
-  Sentry.captureException(new Error('404 hit'));
+import Error from 'next/error'
 
-
-  return <h1>404 - Page Not Found</h1>
+export default function NotFound() {
+  // Opinionated: do not record an exception in Sentry for 404
+  return <Error statusCode={404} />
 }
