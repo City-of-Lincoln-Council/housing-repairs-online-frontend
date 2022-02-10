4196368aa595ebcbb4095293bdfb1f83626e27e8
el <elena@madetech.com>
2022-02-10 14:05:24 +0000

    manually add sentry

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const withSass = require('@zeit/next-sass');
+const { withSentryConfig } = require('@sentry/nextjs');
+
 module.exports = withSass({
   /* bydefault config  option Read For More Optios
   here https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
   cssModules: true
 })
-module.exports = {
+module.exports = withSentryConfig({
   /* Add Your Scss File Folder Path Here */
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -25,5 +27,12 @@ module.exports = {
 
   images: {
     loader: 'default'
-  }
-}
+  },
+},
+{
+  org: 'housing-repairs-online',
+  project: 'housing-repairs-online-frontend',
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  // dryRun: !['dev', 'stg', 'prod'].includes(process.env.ENVIRONMENT),
+  dryRun: false,
+})
